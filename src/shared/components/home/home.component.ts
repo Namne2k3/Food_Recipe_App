219910934173c63aa039c299f8ba899ebbbc3aec
Namne2k3 <nhpn2003@gmail.com>
2025-04-24 09:02:28 +0700

    note & comment code for home.component

--- src/shared/components/home/home.component.ts
+++ src/shared/components/home/home.component.ts
@@ -14,6 +14,14 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './home.component.css'
 })
 export class HomeComponent implements OnInit {
+
+  // Sử dụng signal để lưu trữ dữ liệu
+  // signal là một tính năng mới trong Angular giúp quản lý trạng thái dễ dàng hơn
+
+  // Tại sao lại nên sử dụng signal thay vì khai báo biến thông thường?
+  // Vì khi sử dụng signal, Angular biết chính xác khi nào và cái gì thay đổi, nên có thể cập nhật
+  // UI một cách tự động, chính xác và tối ưu hơn
+
   randomMeal = signal<Meal | null>(null)
   categories = signal<Category[]>([])
   searchTerm = signal<string>('')
@@ -24,7 +32,15 @@ export class HomeComponent implements OnInit {
 
   }
 
+
   ngOnInit(): void {
+    // Trang chủ khi init sẽ sử dụng mealService đã được inject để gọi hàm getRandomMeal
+    // và hàm này sẽ trả về một Observable, vì vậy chúng ta sẽ sử dụng subscribe để nhận dữ liệu
+
+    // Observable là gì?
+    // Observable là một "luồng dữ liệu" mà ta có thể đăng ký  (subscribe) để nhận dữ liệu
+    // khi nó xuất hiện
+    // Đặc biệt nó là một lazy, nghĩa là nó sẽ không thực thi cho đến khi có người đăng ký (subscribe)
     if (this.mealService) {
       this.mealService.getRandomMeal()
         .pipe(
@@ -34,9 +50,13 @@ export class HomeComponent implements OnInit {
           })
         )
         .subscribe((data) => {
-          this.randomMeal.set(data.meals[0]);
+          // Khi bạn set lại giá trị...
+          this.randomMeal.set(data.meals[0]); // => UI sẽ tự cập nhật!
         });
 
+
+      // Sau khi gọi hàm getRandomMeal, chúng ta sẽ gọi hàm getCategories
+      // để lấy danh sách các loại món ăn
       this.mealService.getCategories()
         .pipe(
           catchError((err) => {
